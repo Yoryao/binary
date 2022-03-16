ed2cedee948664004c2f14696ac24f3806ccd6bc
yoryao <jorgejoaquinrivera@gmail.com>
2022-03-16 00:21:23 -0300

    first module - random search - ready

--- app2.js
+++ app2.js
@@ -1,3 +1,5 @@
+
+
 console.log("App Working");
 
 /*
@@ -24,8 +26,8 @@ let arrNumeros = [];
 
 //defino el sorteo del numero.
 const elegirNumero = (index) => {
-  numeroBuscado = parseInt(Math.random() * index);
   numeroIndice = index;
+  numeroBuscado = parseInt(Math.random() * index);
   console.log("Random Number: " + numeroBuscado);
 };
 
@@ -39,10 +41,10 @@ const eleccionUsuario = (index) => {
 const verificarCoincidencia = (objetivo, elegido) => {
   intentos++;
   if (objetivo === elegido) {
-    console.log("Acerto el numero");
-    console.log("Numero elegido acertado: " + numeroUsuario);
+    // arrNumeros.push(numeroUsuario);
+    arrNumeros.push(elegido);
+    console.log("Acerto el numero. Era el " + elegido + ".");
     console.log("Numeros: " + arrNumeros);
-    arrNumeros.push(numeroUsuario);
     arrIntentos.push(intentos);
     console.log("Intentos: " + intentos);
     console.log("----------------------------");
@@ -51,16 +53,16 @@ const verificarCoincidencia = (objetivo, elegido) => {
     numeroBuscado = 0;
     numeroUsuario = 0;
     arrNumeros = [];
-  } else if (arrNumeros.includes(numeroUsuario)) {
+  } else if (arrNumeros.includes(elegido)) {
     intentos--;
     numeroUsuario = parseInt(Math.random() * numeroIndice);
     console.log("Numero ya elegido. Elija Otro: " + numeroUsuario);
-    verificarCoincidencia(numeroBuscado, numeroUsuario);
+    verificarCoincidencia(objetivo, numeroUsuario);
   } else {
+    arrNumeros.push(elegido);
     numeroUsuario = parseInt(Math.random() * numeroIndice);
     console.log("Ese numero no es. Elija Otro: " + numeroUsuario);
-    arrNumeros.push(numeroUsuario);
-    verificarCoincidencia(numeroBuscado, numeroUsuario);
+    verificarCoincidencia(objetivo, numeroUsuario);
   }
 };
 
@@ -70,9 +72,8 @@ const promedio = (array) => {
   for (let i = 0; i < array.length; i++) {
     sumando += array[i];
   }
-
-  console.log(arrIntentos);
   console.log("el promedio es " + sumando / array.length);
+
 let resultado = sumando/array.length;
 let verResultado = document.getElementById("inputGroup-sizing-default")
 verResultado.innerText =`En promedio de acierto es con ${resultado} intentos.`
@@ -88,7 +89,7 @@ const probar = (cantidadPruebas, cantidadNumeros) => {
     //EJECUTO LA PRIMERA ELECCION DEL USUARIO
     eleccionUsuario(cantidadNumeros);
     //COMIENZO LA ITERACION DE LOS NUMEROS PARA ACERTAR
-    verificarCoincidencia(numeroBuscado, numeroUsuario, cantidadNumeros);
+    verificarCoincidencia(numeroBuscado, numeroUsuario);
   }
   promedio(arrIntentos);
 };
@@ -102,10 +103,6 @@ ejecutar.addEventListener("click", () => {
   let cantidadNumeros = document.forms["tabla"]["testRange"].value;
   let cantidadPruebas = document.forms["tabla"]["testQuantity"].value;
 
-  //VERIFICO LA CORRECTA CAPTURA DE LOS DATOS DEL USUARIO
-  console.log("PruebasCantidad: " + cantidadPruebas);
-  console.log("IndiceRango: " + cantidadNumeros);
-
   //EJECUTO LA FUNCION CON LAS VARIABLES DEL USUARIO
-  probar(cantidadNumeros, cantidadPruebas);
+  probar(cantidadPruebas, cantidadNumeros);
 });
